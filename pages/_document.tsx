cb5db7e5a373a514124e4b8191264ec903155489
Benjamin <benjamingurney91@gmail.com>
2023-08-30 14:59:19 +1000

    analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+
 declare var dataLayer: any[];
-
-
 
 class MyDocument extends Document {
   render() {
+    const gtagScriptContent = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-YFTGC0T4Z9');
+    `;
+
     return (
       <Html lang="en">
         <Head>
-        
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-YFTGC0T4Z9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-YFTGC0T4Z9');
-</script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-YFTGC0T4Z9"></script>
+          <script dangerouslySetInnerHTML={{ __html: gtagScriptContent }}></script>
         </Head>
         <body>
           <Main />
